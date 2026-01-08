cd296a46c35586e180dc442a7a16397ede281997
avfitnes <ashokvarma9636@gmail.com>
2026-01-08 19:51:16 +0530

    vercel.json  latest commit

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Easy imports
     },
   },
   server: {
-    historyApiFallback: true,
+    port: 5173,
+    historyApiFallback: true, // React Router refresh fix
   },
 });
